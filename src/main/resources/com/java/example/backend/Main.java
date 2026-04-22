9de66363cdc0ea47c0965b7686dc76aa60c86f6f
AlejandroGutierrezVillalba <guti32128fp@gmail.com>
2026-04-22 13:24:23 +0200

    modificada conexión al main

--- src/main/resources/com/java/example/backend/Main.java
+++ src/main/resources/com/java/example/backend/Main.java
@@ -1,11 +1,11 @@
-package com.lovecode;
+package com.java.example.backend;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class Main 
+public class Main
 {
     public static void main(String[] args) 
     {
